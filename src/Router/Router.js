6b6b875e3bbc617e6e3813793b2ec51984433f37
Subham_subhasis <subhamsubhasis2002@gmail.com>
2022-04-10 20:35:02 +0530

    Added Patient Dashboard

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -3,8 +3,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Login from "../Components/Auth/Login";
 import Signup from "../Components/Auth/Signup";
-import Temp from "../Temp";
-// import Patient from "../Patient/Patient";
+import Patient from "../User/Patient"
 import Receiptionist from "../User/Receiptionist";
 import Head from "../Components/Header";
 import Inventory from "../Inventory/App";
@@ -17,7 +16,7 @@ const Rautes = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        {/* <Route path="/patient" element={<Patient />} /> */}
+        <Route path="/patient" element={<Patient />} />
         <Route path="/doctor" element={<Doctor/>} />
         <Route path="/inventory" element={<Inventory/>}/>
         <Route path="/receiptionist" element={<Receiptionist />} />
